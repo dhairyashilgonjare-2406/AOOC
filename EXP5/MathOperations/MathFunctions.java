cc2532fa2de1ea69a50391385b456688ed6ee4d5
dhairyashilgonjare-2406 <dhairyshilgonjare@gmail.com>
2026-05-08 12:48:52 +0530

    Create MathFunctions.java

---
+++ EXP5/MathOperations/MathFunctions.java
@@ -0,0 +1,16 @@
+package MathOperations;
+
+public class MathFunctions {
+
+    public double findFloor(double num) {
+        return Math.floor(num);
+    }
+
+    public double findCeil(double num) {
+        return Math.ceil(num);
+    }
+
+    public long findRound(double num) {
+        return Math.round(num);
+    }
+}
